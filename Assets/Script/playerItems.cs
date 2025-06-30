131cd1c18994480340e4182c89af551559d9415d
Giuseppe Fallucca <giuseppepiofallucca@gmail.com>
2025-06-30 20:25:24 +0200

    some project fix + updated pTable

--- Assets/Script/playerItems.cs
+++ Assets/Script/playerItems.cs
@@ -5,20 +5,35 @@ public class playerItems : MonoBehaviour
     public GameObject periodicTable;
     public FirstPersonController fpc;
 
-	public bool hasPTable = false;
+    public bool hasPTable = false;
     private bool isMapOpen = false;
-	
+    private bool wasBlocked = false;
 
     void Update()
     {
-		if (!hasPTable) return;
-		
+        if (!hasPTable) return;
+
         if (Input.GetKeyDown(KeyCode.M))
         {
-            fpc.setIsWalking(false);
-            isMapOpen = !isMapOpen;
-            periodicTable.SetActive(isMapOpen); 
-            fpc.playerCanMove = !isMapOpen;
+            if (!isMapOpen)
+            {
+                // Stai aprendo la mappa ora, salvi lo stato attuale
+                wasBlocked = !fpc.playerCanMove;
+                fpc.setIsWalking(false);
+                fpc.playerCanMove = false;
+                periodicTable.SetActive(true);
+                isMapOpen = true;
+            }
+            else
+            {
+                // Stai chiudendo la mappa, ripristini il movimento solo se non era già bloccato
+                if (!wasBlocked)
+                {
+                    fpc.playerCanMove = true;
+                }
+                periodicTable.SetActive(false);
+                isMapOpen = false;
+            }
         }
     }
 }
