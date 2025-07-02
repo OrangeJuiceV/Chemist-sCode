c3e294ebb33dad5862d69e3d9f143d7848f225c2
Giuseppe Fallucca <giuseppepiofallucca@gmail.com>
2025-07-02 23:29:48 +0200

    added hints 1 n 2 enigma

---
+++ Assets/Indizio1.cs
@@ -0,0 +1,43 @@
+using UnityEngine;
+
+public class Indizio1 : MonoBehaviour, IInteractable
+{
+    public Canvas schermataIndizio;
+    public FirstPersonController fpc; // Reference to the player controller
+
+    private bool isOpen = false; // Track if the hint screen is open
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isOpen && Input.GetKeyDown(KeyCode.Escape))
+        {
+            ExitInteraction();
+        }
+    }
+
+    public void Interact()
+    {
+        isOpen = true;
+
+        fpc.setIsWalking(false);
+        fpc.changeActive();
+        fpc.cameraCanMove = false;
+
+        schermataIndizio.gameObject.SetActive(true);
+    }
+
+    public void ExitInteraction()
+    {
+        isOpen = false;
+        fpc.setIsWalking(true);
+        fpc.changeActive();
+        fpc.cameraCanMove = true;
+        schermataIndizio.gameObject.SetActive(false);
+    }
+}
